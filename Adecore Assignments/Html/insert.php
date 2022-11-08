446c8fd7cff047897757eaa32c0d338b96698cda
Kweku-Kai <82861988+Kweku-Kai@users.noreply.github.com>
2022-11-08 15:27:00 +0000

    More code
    Updates to the pre-existing code and an additional file

--- Adecore Assignments/Html/insert.php
+++ Adecore Assignments/Html/insert.php
@@ -2,23 +2,23 @@
 $Firstname = $_POST['Firstname'];
 $Lastname = $_POST['Lastname'];
 $Username = $_POST['Username'];
-$Password = $_POST['Password'];
+$password = $_POST['password'];
 $Contact = $_POST['Contact'];
 $code = $_POST['Code'];
 
-if (!empty($Firstname) || !empty($Lastname) || !empty($Username) ||!empty($Password) || !empty($Contact) || !empty($Code)){
+if (!empty($Firstname) || !empty($Lastname) || !empty($Username) ||!empty($password) || !empty($Contact) || !empty($Code)){
     $host = "localhost";
     $dbUsername = "root";
     $dbpassword = "";
     $dbname = "mine";
 
-    $conn = new mysqli($host, $dbUsername, $dbpassword, $dbname);
+    $conn = new mysqli($host,$dbUsername,$dbpassword,$dbname);
 
     if(mysqli_connect_error()){
         die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
     }else{
         $SELECT = "SELECT Username FROM users WHERE Username = ? Limit 1";
-        $INSERT = "INSERT Into users (Firstname, Lastname, Username, Password, Contact, Code) values(?,?,?,?,?,?)";
+        $INSERT = "INSERT Into users (Firstname, Lastname, Username, password, Contact, Code) values(?,?,?,?,?,?)";
 
         $stmt = $conn->prepare($SELECT);
         $stmt->bind_param("s", $Username);
@@ -30,7 +30,7 @@ if (!empty($Firstname) || !empty($Lastname) || !empty($Username) ||!empty($Passw
         if ($rnum==0){
             $stmt->close();
             $stmt = $conn->prepare($INSERT);
-            $stmt->bind_param("ssssis", $Firstname,$Lastname,$Username,$Password,$Contact,$Code);
+            $stmt->bind_param("ssssis", $Firstname,$Lastname,$Username,$password,$Contact,$Code);
             $stmt->execute();
             echo "New record inserted successfully";
 
---
+++ Adecore Assignments/Html/retrieve.php
@@ -0,0 +1,24 @@
+<?php
+ $Username = $_POST['Username'];
+ $password = $_POST['password'];
+
+ $con = new mysqli("localhost", "root", "", "test");
+ if($con->connect_error) {
+    die("Failed to connect : ".$con->connect_error);
+ }else {
+    $stmt = $con->prepare("select * from users where Username = ?");
+    $stmt->bind_param("s", $Username);
+    $stmt->execute();
+    $stmt_result = $stmt->get_result();
+    if($stmt_result->num_rows > 0){
+        $data = $stmt_result->fetch_assoc();
+        if($data['password'] === $password){
+            echo "Login Succesfully";
+        }else {
+            echo "Invalid Username or Password";
+        }
+    }else {
+        echo "Invalid Username or Password";
+    }
+ }
+?>
